61f605a71ac92f8a71801d5834b2c7e6811c03ef
Ivan Souza - ISU <ivan.souza@iteris.local>
2021-07-09 05:26:34 -0300

    arquivo principal 0-0

---
+++ main.py
@@ -0,0 +1,14 @@
+from lib.file_handling import *
+from lib.interface_functions import *
+
+from time import sleep
+
+# check/create date
+exist("date_users.txt")
+exist("date_contacts.txt")
+
+# check/create main user
+pass
+
+#iniciar no usuario principal
+home_menu()
